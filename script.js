b94982658ddbd7f3421f9b5c17a257332d43294f
aminata <aminata11.traore@uvci.edu.ci>
2026-03-17 17:34:07 +0100

    firts

---
+++ script.js
@@ -0,0 +1,7 @@
+ // Mobile menu toggle
+        const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+        const mobileMenu = document.getElementById('mobileMenu');
+
+        mobileMenuBtn.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
